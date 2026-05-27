661c0049d78611f4c1e006ab6949405d6b223b2b
adityabh18 <adityabhardwaj3112001@gmail.com>
2026-05-27 18:46:15 +0530

    Update socket.js

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SERVER_URL = "http://localhost:8000";
+const SERVER_URL = "https://health-prediction-and-recommendation-8bqr.onrender.com";
 
 export const socket = io(SERVER_URL, {
   withCredentials: true,
