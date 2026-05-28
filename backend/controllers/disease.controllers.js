81bd92b12f1037792adf98eca19b5de623fa0f70
adityabh18 <adityabhardwaj3112001@gmail.com>
2026-05-28 11:41:55 +0530

    Update disease.controllers.js

--- backend/controllers/disease.controllers.js
+++ backend/controllers/disease.controllers.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import {diabetesPrediction, generalPrediction, heartDiseasePrediction, kidneyPrediction } from "../models/disease.models.js";
 
-const mlServer="https://health-prediction-and-recommendation-35bk.onrender.com";
+const mlServer="https://health-prediction-and-recommendation-35bk.onrender.com
 
 /**
  * @desc Predict general disease based on user symptoms
