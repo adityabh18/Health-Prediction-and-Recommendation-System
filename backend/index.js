4767a24f644aa10b9178306e9479b474fd0effb5
adityabh18 <adityabhardwaj3112001@gmail.com>
2026-05-30 14:16:13 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://health-prediction-and-recommendation.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
